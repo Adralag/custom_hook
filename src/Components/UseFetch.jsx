e63ab1c837dfcc6e306b20fa90e78c109951a5e0
Adralag <adedojadaniel@gmail.com>
2025-03-25 13:27:29 +0100

    1

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,13 @@
-const UseFetch = (url) => {
-    
+import {useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    const[data,setData]=useState();
+    useEffect(()=>{
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+return [data]
 }
 
-export default UseFetch
+export default useFetch
